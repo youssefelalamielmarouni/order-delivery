2c54c96985ab4295c2b7a3f1de337a1169cbe0be
Youssef <youssefalami664@email.com>
2026-01-31 14:06:18 +0100

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
-Route::middleware('auth:api')->get('/me', function () {
-    return auth()->user();
+Route::middleware('auth:api')->group(function () {
+    // Products (Admin Only)
+    Route::middleware('role:admin')->apiResource('products', ProductController::class);
+
+    // Orders
+    Route::get('orders', [OrderController::class,'index']); // All orders
+    Route::post('orders', [OrderController::class,'store']); // Client create order
+    Route::patch('orders/{order}/status', [OrderController::class,'updateStatus']); // Admin / Delivery update status
 });
